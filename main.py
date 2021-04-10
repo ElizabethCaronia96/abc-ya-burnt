5ca4543cca7537ee60ed970c372cb78c1b65f77a
Elizabeth Caronia <elizabethcaronia@Elizabeths-Air.fios-router.home>
2021-04-09 23:03:16 -0400

    change

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+def app(environ, start_response):
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
 
 @app.route('/')
 def index():
