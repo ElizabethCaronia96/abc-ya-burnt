2dad55c2d18a6a853dd6f26e4fb712a83328e955
Elizabeth Caronia <elizabethcaronia@Elizabeths-Air.fios-router.home>
2021-04-23 18:11:22 -0400

    add django as req

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -1,4 +1,14 @@
-from main import app
+"""
+WSGI config for gettingstarted project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
 
-if __name__ == "__main__":
-    app.run()
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
